72de5432a73c74ff677914c7cb5b523e0ab94b1d
Aman11b <amansingh1100b@gmail.com>
2026-06-22 03:18:01 +0530

    useEffect and Fetch API

--- src/starter/08-fetch-data/index.tsx
+++ src/starter/08-fetch-data/index.tsx
@@ -1,4 +1,39 @@
+import { useState, useEffect } from "react";
+const url = "https://www.course-api.com/react-tours-project";
+
 function Component() {
+  // tours
+  const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchDate = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Failed tp fetch tours`);
+        }
+        const rawDate = await response.json();
+        console.log(rawDate);
+      } catch (error) {
+        const message =
+          error instanceof Error ? error.message : "there was an error";
+        setIsError(message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchDate();
+  }, []);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return <h3>Error... {isError}</h3>;
+  }
   return (
     <div>
       <h2>React & Typescript</h2>
